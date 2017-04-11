cbdec37ca056e49edc906a9c5c9759618ecdebb5
Lan Xiao <lx223@users.noreply.github.com>
2017-04-11 23:34:09 +0100

    Fix the command that sets zsh as default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ git config --global user.name "Lan Xiao"
 git config --global user.email "lx223@users.noreply.github.com"
 git config --global credential.helper osxkeychain
 
-# Zsh
-brew install zsh zplug
-ln -s "$(pwd)/.zshrc" ~/.zshrc
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
-
 # iTerm2
 brew tap caskroom/cask
 brew cask install iterm2
 git submodule update
+
+# Zsh
+brew install zsh zplug
+ln -s "$(pwd)/.zshrc" ~/.zshrc
+(echo 'echo /usr/local/bin/zsh >> /etc/shells' | sudo -s) && chsh -s /usr/local/bin/zsh
